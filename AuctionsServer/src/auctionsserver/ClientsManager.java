ea9d123fdf4d778680062340ff46373d9f802366
Andre <redan.blue27@gmail.com>
2016-12-18 13:31:56 +0000

    Separated Server-Client Source into its own projects for ease of building. Renamed both projects to indicate that they should compile into two different executables.

---
+++ AuctionsServer/src/auctionsserver/ClientsManager.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auctionsserver;
+
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Every ClientsManager is a manager for an auction house.
+ * It manages accesses to all relevant client data
+ * and also writer threads' queues of messages to write out
+ * to clients.
+ * @author Andre
+ */
+public class ClientsManager {
+    private final Map<String,String> clients;
+    private final Map<String,Socket> activeClients;
+    private final Map<Long,List<String>> bidders;
+    private final Map<Long,String> highestBidders;
+    private final Map<String,BlockingQueue<String>> writeQueues;
+    
+    public ClientsManager() 
+    {
+        activeClients = new HashMap<>();
+        bidders = new HashMap<>();
+        highestBidders = new HashMap<>();
+        clients = new HashMap<>();
+        writeQueues=new HashMap<>();
+    }
+
+    boolean registerUser(String user, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean loginUser(String user, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void listClients(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void registerBid(long hash, float value, String user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void registerAuction(String user, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
